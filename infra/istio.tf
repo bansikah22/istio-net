c7835bceaf70f0f137d287721acf7dd99c85a893
nta <NoelBansikah.Tandap@adorsys.com>
2026-04-05 23:24:05 +0100

    chore: optimize infra code

--- infra/istio.tf
+++ infra/istio.tf
@@ -2,6 +2,7 @@ resource "helm_release" "istio_base" {
   name       = "istio-base"
   repository = "https://istio-release.storage.googleapis.com/charts"
   chart      = "base"
+  version    = "1.17.2"
   namespace  = "istio-system"
   create_namespace = true
 }
@@ -10,6 +11,7 @@ resource "helm_release" "istiod" {
   name       = "istiod"
   repository = "https://istio-release.storage.googleapis.com/charts"
   chart      = "istiod"
+  version    = "1.17.2"
   namespace  = "istio-system"
 
   depends_on = [helm_release.istio_base]
@@ -19,6 +21,7 @@ resource "helm_release" "istio_ingress" {
   name       = "istio-ingress"
   repository = "https://istio-release.storage.googleapis.com/charts"
   chart      = "gateway"
+  version    = "1.17.2"
   namespace  = "istio-system"
 
   depends_on = [helm_release.istiod]
--- infra/main.tf
+++ infra/main.tf
@@ -5,7 +5,7 @@ module "vpc" {
   name = "${var.cluster_name}-vpc"
 
   cidr = "10.0.0.0/16"
-  azs  = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  azs  = ["${var.aws_region}a", "${var.aws_region}b", "${var.aws_region}c"]
 
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
